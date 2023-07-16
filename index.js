b93d93869c6a352482d8c91213c983969716fd82
Po Jui Juan <61130768+bray851011@users.noreply.github.com>
2023-07-16 19:50:40 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const textContainer = document.getElementById('changing-text');
+const strings = ["programmer", "challenger"]; // The strings to switch between
+let currentIndex = 0; // Index to keep track of the current string
+
+function switchText() {
+    textContainer.style.opacity = 0; // Fade out
+    setTimeout(() => {
+        textContainer.textContent = strings[currentIndex];
+        textContainer.style.opacity = 1; // Fade in
+        currentIndex = (currentIndex + 1) % strings.length; // Increment index and wrap around
+    }, 500); // Delay before fading in
+}
+
+setInterval(switchText, 3000); // Switch every 3 seconds (3000 milliseconds)
